624aa26f7deea6d743859bcd2f5e7a255485c5ef
gunn3r71 <lucasgunner3@gmail.com>
2021-05-14 23:57:09 -0300

    Finalizando

--- ExercicioDIOTriangulo/Program.cs
+++ ExercicioDIOTriangulo/Program.cs
@@ -6,7 +6,22 @@ namespace ExercicioDIOTriangulo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double a, b, c;
+            string[] valor = Console.ReadLine().Split();
+            a = Convert.ToDouble(valor[0]);
+            b = Convert.ToDouble(valor[1]);
+            c = Convert.ToDouble(valor[2]);
+
+            if (((a-b) < c && (c < a+b)) && ((a-c) < b && (b < a-c)) && ((b-c) < a && (a < (b+c)))) //complete a condicional
+            {
+                double Perimetro = a + b + c;
+                Console.WriteLine("Perimetro = {0:0.0}", Perimetro); //complete a saida
+            }
+            else
+            {
+                double Area = ((a + b) * c) / 2;
+                Console.WriteLine("Area = {0:0.0}", Area); //complete a saida
+            }
         }
     }
 }
