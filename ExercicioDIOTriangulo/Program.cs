0dba64cc492e66e104685ef68c72fa4b28e8cc4f
gunn3r71 <lucasgunner3@gmail.com>
2021-05-14 23:32:14 -0300

    Adicionar arquivos de projeto.

---
+++ ExercicioDIOTriangulo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExercicioDIOTriangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
